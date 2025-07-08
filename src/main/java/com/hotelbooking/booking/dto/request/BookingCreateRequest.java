7ad3eaf140527b7804935caa6843e991b6b31826
RashadHamidli <online.carats@gmail.com>
2025-07-08 20:21:11 +0400

    changed project

--- src/main/java/com/hotelbooking/booking/dto/request/BookingCreateRequest.java
+++ src/main/java/com/hotelbooking/booking/dto/request/BookingCreateRequest.java
@@ -1,19 +1,35 @@
 package com.hotelbooking.booking.dto.request;
 
+import com.hotelbooking.booking.entity.BookingStatus;
+import com.hotelbooking.booking.validation.ValidDateRange;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@ValidDateRange
 public class BookingCreateRequest {
+    @NotBlank
+    @Length(max = 50, min = 2)
     String customerName;
+    @NotNull
+    @Email
     String customerEmail;
-    LocalDateTime startDate;
-    LocalDateTime endDate;
-    String status;
+    @NotNull
+    @FutureOrPresent
+    LocalDate startDate;
+    @NotNull
+    @FutureOrPresent
+    LocalDate endDate;
+    @NotNull
+    BookingStatus status = BookingStatus.ACTIVE;
+    @NotNull
     Long roomId;
 }
