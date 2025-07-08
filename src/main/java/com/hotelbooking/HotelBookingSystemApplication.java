10056fdb77abf8f4d2f902adb6bbb7ec2f442899
RashadHamidli <online.carats@gmail.com>
2025-07-08 18:18:31 +0400

    all changed project

---
+++ src/main/java/com/hotelbooking/HotelBookingSystemApplication.java
@@ -0,0 +1,15 @@
+package com.hotelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class HotelBookingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelBookingSystemApplication.class, args);
+    }
+
+}
